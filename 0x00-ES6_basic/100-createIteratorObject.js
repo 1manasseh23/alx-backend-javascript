fe7526531df0390ccba54c74b8d5cfda1f0bf61f
1manasseh23 <cletavivan@gmail.com>
2024-06-26 06:28:30 +0000

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+const createIteratorObject = (report) => {
+  const { allEmployees } = report;
+  const arr = [];
+
+  for (const key of Object.keys(allEmployees)) {
+    arr.push(...allEmployees[key]);
+  }
+  return arr;
+};
+
+export default createIteratorObject;
