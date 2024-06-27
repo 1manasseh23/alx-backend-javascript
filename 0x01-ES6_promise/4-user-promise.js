de278669d12c7e11a1f4c189d45e7b53e4ce98cc
1mansseh23 <cletavivan@gmail.com>
2024-06-26 17:40:49 -0700

    Simple promise

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,8 @@
+function signUpUser(firstName, lastName) {
+    return new Promise((resolve, reject) => {
+      // Simulate successful signup (replace with actual signup logic)
+      resolve({ firstName, lastName });
+    });
+  }
+  
+  export default signUpUser;
