5a8e1d9e4ac69ea8150aac3fb9e52d391c3e0b36
1manasseh23 <cletavivan@gmail.com>
2024-06-27 16:55:01 +0000

    ou used to attend a place like

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,3 +1,6 @@
+/*This line declares a class named ClassRoom
+ * and marks it for export using the default keyword
+ */
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
