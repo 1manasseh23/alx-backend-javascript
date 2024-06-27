484aeb358a51a2bfcfccadd7f1ef05e01359e305
1mansseh23 <cletavivan@gmail.com>
2024-06-26 17:33:54 -0700

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+import uploadPhoto from './utils'; // Assuming utils.js exports both functions
+import createUser from './utils'; // Assuming utils.js exports both functions
+
+async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+
+    console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
+}
+
+export default handleProfileSignup;
