146f612204fb3b219d96cd6b9de086adee62d2bb
1mansseh23 <cletavivan@gmail.com>
2024-06-26 17:50:46 -0700

    Simple promise

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,4 @@
+export default function uploadPhoto(fileName) {
+    // Simulate a rejection (replace with actual upload logic)
+    return Promise.reject(new Error(`${fileName} cannot be processed`));
+  }
