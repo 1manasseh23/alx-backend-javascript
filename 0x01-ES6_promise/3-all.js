eb96d13a9a7e6435c944defdc6254ff0ecef1fa1
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-06-28 18:57:33 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,12 +3,13 @@ import { uploadPhoto, createUser } from './utils'; // Assuming utils.js exports 
 
 async function handleProfileSignup() {
   try {
-    const [photoResponse, userResponse] = await Promise.all([
+    // const [photoResponse, userResponse] = await 
+      return Promise.all([
       uploadPhoto(),
       createUser(),
-    ]);
+    ]).then(([uploadPhoto, createUser]) => {console.log(uploadPhoto.body, createUser.firstName, createUser.lastName);})
 
-    console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    // console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
   } catch (error) {
     console.error('Signup system offline');
   }
