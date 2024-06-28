9eff9f2c20a0aaba0587b131f543b2413ad5e71f
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-06-28 22:41:15 +0100

    clear

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,29 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
-    .all([uploadPhoto(), createUser()])
-    .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
-}
-/*
-import { uploadPhoto, createUser } from './utils'; // Assuming utils.js exports both functions
-// import createUser from './utils'; Assuming utils.js exports both functions
-
-async function handleProfileSignup() {
-  try {
-    // const [photoResponse, userResponse] = await 
-      return Promise.all([
-      uploadPhoto(),
-      createUser(),
-    ]).then(([uploadPhoto, createUser]) => { console.log(uploadPhoto.body, createUser.firstName, createUser.lastName); })
-
-    // console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
-  } catch (error) {
-    console.error('Signup system offline');
-  }
-}
-
-export default handleProfileSignup;
-*/
+  .all([uploadPhoto(), createUser()])
+  .then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+  .catch(() => console.log('Signup system offline'));
+};
