d8703054d2fe0656e2dfc931fdc54b08e27cf25c
1mansseh23 <cletavivan@gmail.com>
2024-07-03 11:26:10 -0700

    String literal types

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,90 @@
+// DirectorInterface.ts
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// TeacherInterface.ts
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Director.ts
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+// Teacher.ts
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// createEmployee.ts
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+
+  // console.log(createEmployee(200));
+  // console.log(createEmployee(1000));
+  // console.log(createEmployee('$500'));
+
+  // 6. Creating functions specific to employees 
+  // isDirector.ts
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is DirectorInterface {
+  return (employee as DirectorInterface).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+// executeWork(createEmployee(200));
+// executeWork(createEmployee(1000));
+
+// 7. String literal types
+// Subjects.ts
+type Subjects = 'Math' | 'History';
+
+// teachClass.ts
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    throw new Error('Invalid subject: ${todayClass}');
+  }
+}
+
+// teachClass('Math');
+// teachClass('History');
