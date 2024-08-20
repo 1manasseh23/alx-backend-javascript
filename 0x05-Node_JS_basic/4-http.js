e5e296f6f0728c307c6b1d8556983768738f5303
1mansseh23 <cletavivan@gmail.com>
2024-08-20 14:56:59 -0700

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -18,19 +18,4 @@ app.listen(PORT, HOST, () => {
   process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
-/* // Create the HTTP server
-// const app = http.createServer((req, res) => {
-//     // Set the response header to plain text
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
-//     // Write the response body
-//     res.write('Hello Holberton School!');
-    
-//     // End the response
-//     res.end();
-// });
-
-// // The server listens on port 1245
-// app.listen(1245);
-*/
 module.exports = app;
