11becd6d079c20cbb420f62aec7efa64664e7604
1mansseh23 <cletavivan@gmail.com>
2024-08-20 14:25:40 -0700

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,3 @@
-/*
-* Display the massage welcome to school
-*/
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
@@ -10,21 +7,3 @@ process.stdin.on('data', (data) => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-// // constand reading the line
-// const readline = require('readline');
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// // Display the massage welcome to school
-// console.log("Welcome to Holberton School, what is your name?");
-
-
-// rl.on('line', (name) => {
-//     console.log(`Your name is: ${name}`);
-//     rl.close();
-//     console.log("This important software is now closing");
-// });
