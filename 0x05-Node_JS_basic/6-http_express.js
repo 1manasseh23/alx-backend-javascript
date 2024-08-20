6ab511006aba33f2932d9d61f5c85a86bc5b1327
1mansseh23 <cletavivan@gmail.com>
2024-08-20 15:13:25 -0700

    Create a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,16 +1,16 @@
-// Require the express module to cread thr server
 const express = require('express');
 
 // Create an instance of Express
 const app = express();
+const PORT = 1245;
 
 // Define the route for the root path "/"
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.send('Hello Holberton School!');
 });
 
 // The server listens on port 1245
-app.listen(1245, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 1245');
 });
 
