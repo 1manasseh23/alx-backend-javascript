2554a689a3fdc4b1fe2a5f6639285ce87f417bb0
1mansseh23 <cletavivan@gmail.com>
2024-08-20 14:35:05 -0700

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -37,32 +37,5 @@ const countStudents = (dataPath) => {
       console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
     }
   };
-// function countStudents(path) {
-//     try {
-//         const data = fs.readFileSync(path, 'utf8');
-
-//         const lines = data.split('\n').filter((line) => line.length > 0);
-
-//         const headers = lines.shift();
-
-//         const studentsByField = {};
-
-//         lines.forEach((line) => {
-//             const [firstname, lastname, age, field] = line.split(',');
-//             if (!studentsByField[field]) {
-//                 studentsByField[field] = [];
-//             }
-//             studentsByField[field].push(firstname);
-//         });
-//         console.log(`Number of students: ${lines.length}`);
-
-//         for (const [field, students] of Object.entries(studentsByField)) {
-//             console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-//         }
-
-//     } catch (error) {
-//         throw new Error('Cannot load the database');
-//     }
-// }
 
 module.exports = countStudents;
