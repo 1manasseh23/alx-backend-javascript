9ae336969168ae1b308151a78ddb6ac7e9c0dc03
1mansseh23 <cletavivan@gmail.com>
2024-08-20 14:11:56 -0700

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,9 @@
-// This prints in STDOUT the string argument
-
+/*
+* This prints in STDOUT the string argument
+*/
 function displayMessage(message) {
     console.log(message);
 }
 
+// Export the function
 module.exports = displayMessage;
